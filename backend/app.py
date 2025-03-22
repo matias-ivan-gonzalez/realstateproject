0480ecbcacd179317cf36952a6e6f46ba5986d47
Matias <maseruma@gmail.com>
2025-03-22 00:57:48 -0600

    refactor

--- backend/app.py
+++ backend/app.py
@@ -2,8 +2,11 @@ from flask import Flask, render_template
 import os
 
 
-app = Flask(__name__, template_folder=os.path.join('..', 'templates'))
-
+app = Flask(
+    __name__,
+    static_folder=os.path.join('..', 'static'),  # Ruta a la carpeta 'static'
+    template_folder=os.path.join('..', 'templates')  # Ruta a la carpeta 'templates'
+)
 # Ruta para la página principal
 @app.route('/')
 def index():
