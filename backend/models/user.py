ef6adb12f632200a016af6fa1a02344ac05b93aa
Matias Gonzalez <matiasivan.gonzalez@mercadolibre.com>
2025-05-27 21:29:43 -0600

    Fixes Mati (#71)
    * agrego confirmacion al cerrar sesion
    
    * validacion de tarjeta de credito con 16 digitos visa o mastercard
    
    * cambios en la relacion dni - nacionalidad
    
    ---------
    
    Co-authored-by: Matias <maseruma@gmail.com>

--- backend/models/user.py
+++ backend/models/user.py
@@ -8,13 +8,17 @@ class Usuario(db.Model):
     tipo = db.Column(db.String(50))  # STI: cliente, administrador, etc.
     nombre = db.Column(db.String(100), nullable=False)
     apellido = db.Column(db.String(100), nullable=False)
-    dni = db.Column(db.String(20), unique=True, nullable=False)
+    dni = db.Column(db.String(20), nullable=False)
     email = db.Column(db.String(100), unique=True, nullable=False)
     contrasena = db.Column(db.String(100), nullable=False)
     telefono = db.Column(db.String(20), nullable=False)
     nacionalidad = db.Column(db.String(50), nullable=False)
     eliminado = db.Column(db.Boolean, default=False, nullable=False)
     
+    # Índice único compuesto para dni y nacionalidad
+    __table_args__ = (
+        db.UniqueConstraint('dni', 'nacionalidad', name='uix_dni_nacionalidad'),
+    )
     
     rol_id = db.Column(db.Integer, db.ForeignKey('rol.id'))
 
