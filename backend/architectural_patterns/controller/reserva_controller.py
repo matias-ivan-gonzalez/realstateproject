56c506a6da5a1d113cd6813038364ac813001af0
Matias <maseruma@gmail.com>
2025-07-05 01:35:17 -0600

    Reapply "envio cambios iniciales"
    This reverts commit fff6b4f02ae94d777f1e23619500f2bb7d4a3835.

--- backend/architectural_patterns/controller/reserva_controller.py
+++ backend/architectural_patterns/controller/reserva_controller.py
@@ -28,7 +28,8 @@ class ReservaController:
                 propiedad_id=propiedad.id,
                 fecha_inicio=fecha_inicio_dt,
                 fecha_fin=fecha_fin_dt,
-                cantidad_personas=cantidad_huespedes
+                cantidad_personas=cantidad_huespedes,
+                reembolsable=propiedad.reembolsable
             )
             db.session.add(reserva)
             db.session.commit()
@@ -144,7 +145,8 @@ class ReservaController:
             propiedad_id=propiedad.id,
             fecha_inicio=fecha_inicio_dt,
             fecha_fin=fecha_fin_dt,
-            cantidad_personas=cantidad_huespedes
+            cantidad_personas=cantidad_huespedes,
+            reembolsable=propiedad.reembolsable
         )
         db.session.add(reserva)
         db.session.commit()
